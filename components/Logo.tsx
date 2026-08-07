04cd27800f4b6e4f5bad96b791adc281fffcf0d0
rafaelhbello <rafael.bello.2@hotmail.com>
2026-08-07 18:10:09 -0300

    Refina SVG da logo LUMI para ficar mais fiel ao design original

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,30 +1,71 @@
 type LogoProps = {
   className?: string;
-  markOnly?: boolean;
   light?: boolean;
 };
 
-export default function Logo({ className = 'h-8 w-8', markOnly = false, light = false }: LogoProps) {
+export default function Logo({ className = 'h-8 w-8', light = false }: LogoProps) {
   const navy = light ? '#FFFFFF' : '#0F2744';
   const gold = '#C8A24C';
-  const goldDark = '#AD8838';
+  const goldMid = '#B8943F';
+  const goldDark = '#9A7A30';
 
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 80 80"
+      viewBox="0 0 100 100"
       fill="none"
       className={className}
       aria-hidden
     >
-      {/* Navy geometric base */}
+      {/* Navy open hexagon / C shape */}
       <path
-        d="M52 12 L28 12 L12 32 L12 48 L28 68 L52 68 L52 54 L34 54 L26 46 L26 34 L34 26 L52 26 Z"
+        d="M68 14
+           H36
+           L16 38
+           V62
+           L36 86
+           H68
+           V70
+           H42
+           L30 58
+           V42
+           L42 30
+           H68
+           Z"
         fill={navy}
       />
-      {/* Gold accent */}
-      <path d="M42 8 L54 8 L64 24 L54 40 L42 40 L50 24 Z" fill={gold} />
-      <path d="M54 8 L66 8 L76 24 L66 40 L54 40 L62 24 Z" fill={goldDark} />
+      {/* Gold ribbon - left panel */}
+      <path
+        d="M52 10
+           L66 10
+           L78 34
+           L66 58
+           L52 58
+           L62 34
+           Z"
+        fill={gold}
+      />
+      {/* Gold ribbon - right panel (darker for depth) */}
+      <path
+        d="M66 10
+           L80 10
+           L92 34
+           L80 58
+           L66 58
+           L76 34
+           Z"
+        fill={goldDark}
+      />
+      {/* Gold ribbon highlight */}
+      <path
+        d="M62 34
+           L76 34
+           L66 58
+           L52 58
+           Z"
+        fill={goldMid}
+        opacity="0.85"
+      />
     </svg>
   );
 }
